46a6518478e1900571a6492cf5c69fea4452e393
Ryan <sirm2zisv@gmail.com>
2020-12-18 13:48:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const copyFile = () => {
   fs.copyFile(
-    path.resolve(process.cwd(), './CNAME'),
+    path.resolve(process.cwd(), './src/CNAME'),
     path.resolve(process.cwd(), './dist/CNAME2'),
     (err) => { if (err) { throw err } }
   );
